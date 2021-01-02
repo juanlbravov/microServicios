8a22522e8be5f1717c4ca804324bce0421106c49
juanlbravov <juanlbravov@gmail.com>
2021-01-02 23:17:02 +0100

    controlador rest de los productos

--- stsWorkSpace/springboot-servicio-productos/src/main/java/com/microservicios/springboot/app/productos/SpringbootServicioProductosApplication.java
+++ stsWorkSpace/springboot-servicio-productos/src/main/java/com/microservicios/springboot/app/productos/SpringbootServicioProductosApplication.java
@@ -1,20 +1,11 @@
 package com.microservicios.springboot.app.productos;
 
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
-
 public class SpringbootServicioProductosApplication {
-
-
 	public static void main(String[] args) {
-
 		SpringApplication.run(SpringbootServicioProductosApplication.class, args);
-
 	}
-
-
 }
