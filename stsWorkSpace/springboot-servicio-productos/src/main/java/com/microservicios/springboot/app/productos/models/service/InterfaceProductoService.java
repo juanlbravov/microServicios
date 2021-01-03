3e29ee01a873a8832d5cb3a5f27a60e3cf2bd16e
Juan Luis Bravo V <juanlbravov@users.noreply.github.com>
2021-01-03 17:30:40 +0100

    actualizo InterfaceProductoService.java

--- stsWorkSpace/springboot-servicio-productos/src/main/java/com/microservicios/springboot/app/productos/models/service/InterfaceProductoService.java
+++ stsWorkSpace/springboot-servicio-productos/src/main/java/com/microservicios/springboot/app/productos/models/service/InterfaceProductoService.java
@@ -2,6 +2,9 @@
  * Clase service para los productos
  * @author Juan Luis Bravo Velez
  * 
+ * interface implementada para ProductoDao.java
+ *  com.microservicios.springboot.app.productos.models.dao
+ *
  */
 
 package com.microservicios.springboot.app.productos.models.service;
@@ -9,9 +12,6 @@ package com.microservicios.springboot.app.productos.models.service;
 import java.util.List;
 import com.microservicios.springboot.app.productos.models.entity.Producto;
 
-/** interface implementada para ProductoDao.java
- *  com.microservicios.springboot.app.productos.models.dao
- */
 public interface InterfaceProductoService {
 	
 	public List<Producto> findAll();
