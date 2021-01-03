b418c64a40ee01398a2784bd1200330309f9a085
Juan Luis Bravo V <juanlbravov@users.noreply.github.com>
2021-01-03 17:24:04 +0100

    actualizo SpringbootServicioProductosApplication
    añado comentario

--- stsWorkSpace/springboot-servicio-productos/src/main/java/com/microservicios/springboot/app/productos/SpringbootServicioProductosApplication.java
+++ stsWorkSpace/springboot-servicio-productos/src/main/java/com/microservicios/springboot/app/productos/SpringbootServicioProductosApplication.java
@@ -1,3 +1,8 @@
+/** 1
+ *  Creando el repositorio PRINCIPAL para los productos
+ *  
+ */
+  
 package com.microservicios.springboot.app.productos;
 
 import org.springframework.boot.SpringApplication;
@@ -5,7 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootServicioProductosApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootServicioProductosApplication.class, args);
 	}
+
 }
