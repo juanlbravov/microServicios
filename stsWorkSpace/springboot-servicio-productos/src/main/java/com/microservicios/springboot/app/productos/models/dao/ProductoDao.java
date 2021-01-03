3968beb628e62381ec018106ed74e4a52af38ae1
Juan Luis Bravo V <juanlbravov@users.noreply.github.com>
2021-01-03 17:27:01 +0100

    actualizar ProductoDao

--- stsWorkSpace/springboot-servicio-productos/src/main/java/com/microservicios/springboot/app/productos/models/dao/ProductoDao.java
+++ stsWorkSpace/springboot-servicio-productos/src/main/java/com/microservicios/springboot/app/productos/models/dao/ProductoDao.java
@@ -9,7 +9,6 @@
 package com.microservicios.springboot.app.productos.models.dao;
 
 import org.springframework.data.repository.CrudRepository;
-
 import com.microservicios.springboot.app.productos.models.entity.Producto;
 
 public interface ProductoDao extends CrudRepository<Producto, Long>{
